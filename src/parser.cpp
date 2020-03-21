454117aa10cd37dd7c025d5abfc7bc040db9ce89
mu001999 <mu001999@outlook.com>
2020-03-21 09:45:39 +0800

    update

--- src/parser.cpp
+++ src/parser.cpp
@@ -7,27 +7,27 @@ using namespace parsec;
 namespace hol2cpp
 {
 // the grammer is tentative now
-Parser::Parser()
-  : digit_(Token::by(::isdigit)),
-    alpha_(Token::by(::isalpha)),
-    blank_(Token::by(::isblank)),
+Parser::Parser() :
 
-    // blanks := blank blanks | blank
-    blanks_((blank_ + blanks_ >> [](char blk, std::string blks) {
-        return std::string();
-    }) | (blank_ >> [](char blk) { return std::string(); })),
+digit_(Token::by(::isdigit)),
+alpha_(Token::by(::isalpha)),
+blank_(Token::by(::isblank)),
 
-    // identifier := alpha identifier | alpha
-    identifier_((alpha_ + identifier_ >> [](char head, std::string tail) {
-        return head + tail;
-    }) | (alpha_) >> [](char alpha) { return std::string(0, alpha); }),
+// blanks := blank blanks | blank
+blanks_((blank_ + blanks_ >> [](char blk, std::string blks) {
+    return std::string();
+}) | (blank_ >> [](char blk) { return std::string(); })),
 
-    // fun_decl_pre := "fun" blanks identifier blanks "::" blanks
-    fun_decl_pre_("fun"_T + blanks_ + identifier_ + blanks_ + "::"_T + blanks_ >>
-        [](string, string, string ident, string, string, string) { return ident; })
-{
+// identifier := alpha identifier | alpha
+identifier_((alpha_ + identifier_ >> [](char head, std::string tail) {
+    return head + tail;
+}) | (alpha_) >> [](char alpha) { return std::string(0, alpha); }),
 
-}
+// fun_decl_pre := "fun" blanks identifier blanks "::" blanks
+fun_decl_pre_("fun"_T + blanks_ + identifier_ + blanks_ + "::"_T + blanks_ >>
+    [](string, string, string ident, string, string, string) { return ident; })
+
+{}
 
 unique_ptr<FunDecl>
 Parser::pas_fun_decl(const string &str)
