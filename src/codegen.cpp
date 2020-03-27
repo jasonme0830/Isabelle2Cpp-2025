11ed6b0fe31170898090b33cffcedda042b78d59
mu001999 <mu001999@outlook.com>
2020-03-27 15:08:39 +0800

    update codegen

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -2,12 +2,37 @@
 #include "code.hpp"
 #include "context.hpp"
 
+using namespace std;
+
 namespace hol2cpp {
 AST::~AST() = default;
 Type::~Type() = default;
 Expr::~Expr() = default;
 
+void NormalType::gen_typeinfos(Context &context) const {
+  context.add_normal_type(name);
+}
+
+void ArgumentType::gen_typeinfos(Context &context) const {
+  context.add_argument_type(name);
+}
+
+void TemplateType::gen_typeinfos(Context &context) const {
+  context.add_template_type(name);
+  arg->gen_typeinfos(context);
+}
+
+void FuncType::gen_typeinfos(Context &context) const {
+  for (size_t i = 0; i < types.size() - 1; ++i) {
+    context.entry_argument_type(i);
+    types[i]->gen_typeinfos(context);
+  }
+  context.entry_result_type();
+  result_type()->gen_typeinfos(context);
+}
+
 void FuncDecl::codegen(Context &context, Code &code) const {
-  auto result_type = type->result_type();
+  code.entry_func();
+  type->gen_typeinfos(context);
 }
 }
