d263bda6e3fe3e81ad73987772a66d102c55fdff
mu001999 <mu001999@outlook.com>
2020-03-27 15:08:25 +0800

    update code

--- src/code.hpp
+++ src/code.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <iostream>
+
 namespace hol2cpp {
 class Code {
   public:
-    Code() = default;
+    Code() : entry_func_(-1), indent_(0) {}
+
+    void entry_func() {
+      ++entry_func_;
+    }
+
+    void print(std::ostream &out = std::cout);
 
   private:
+    int entry_func_;
+    int indent_;
 };
 }
