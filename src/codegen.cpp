67341cb4c60f1395b0b3a094d452004cafac1637
mu001999 <mu001999@outlook.com>
2020-03-28 16:10:47 +0800

    update codegen

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -1,6 +1,5 @@
 #include "ast.hpp"
 #include "code.hpp"
-#include "context.hpp"
 
 using namespace std;
 
@@ -9,30 +8,58 @@ AST::~AST() = default;
 Type::~Type() = default;
 Expr::~Expr() = default;
 
-void NormalType::gen_typeinfos(Context &context) const {
-  context.add_normal_type(name);
-}
+void NormalType::gen_typeinfo(FuncEntity &entity) const {
+  static map<string, string> mapping {
+    { "nat", "std::uint64_t" },
+    { "int", "std::int64_t" }
+  };
 
-void ArgumentType::gen_typeinfos(Context &context) const {
-  context.add_argument_type(name);
-}
-
-void TemplateType::gen_typeinfos(Context &context) const {
-  context.add_template_type(name);
-  arg->gen_typeinfos(context);
-}
-
-void FuncType::gen_typeinfos(Context &context) const {
-  for (size_t i = 0; i < types.size() - 1; ++i) {
-    context.entry_argument_type(i);
-    types[i]->gen_typeinfos(context);
+  if (mapping.count(name)) {
+    entity.add_normal_type(mapping[name]);
+  } else {
+    entity.add_normal_type(name);
   }
-  context.entry_result_type();
-  result_type()->gen_typeinfos(context);
 }
 
-void FuncDecl::codegen(Context &context, Code &code) const {
+void ArgumentType::gen_typeinfo(FuncEntity &entity) const {
+  entity.add_argument_type(name);
+}
+
+void TemplateType::gen_typeinfo(FuncEntity &entity) const {
+  static map<string, string> mapping {
+    { "set", "std::set" },
+    { "option", "std::optional" },
+    { "list", "std::list" }
+  };
+
+  if (mapping.count(name)) {
+    entity.add_template_type(mapping[name]);
+  } else {
+    entity.add_template_type(name);
+  }
+  arg->gen_typeinfo(entity);
+}
+
+void FuncType::gen_typeinfo(FuncEntity &entity) const {
+  entity.entry_type();
+  result_type()->gen_typeinfo(entity);
+  for (size_t i = 0; i < types.size() - 1; ++i) {
+    entity.entry_type();
+    types[i]->gen_typeinfo(entity);
+  }
+}
+
+void VarExpr::codegen(Code &code) const {
+
+}
+
+void ConsExpr::codegen(Code &code) const {
+
+}
+
+void FuncDecl::codegen(Code &code) const {
   code.entry_func();
-  type->gen_typeinfos(context);
+  code.current_entity().set_name(name);
+  type->gen_typeinfo(code.current_entity());
 }
 }
