a04f6eafb77fa911d88698df80dbec1861168293
mu001999 <mu001999@outlook.com>
2020-03-28 16:07:50 +0800

    update as funcentity

---
+++ src/funcentity.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+
+namespace hol2cpp {
+class FuncEntity {
+  public:
+    FuncEntity() = default;
+
+    const std::string &name() const {
+      return name_;
+    }
+
+    void set_name(std::string name) {
+      name_ = name;
+    }
+
+    void entry_type() {
+      entry_type_ = types_.size();
+      types_.emplace_back();
+      anchor_ = 0;
+    }
+
+    void add_normal_type(std::string name) {
+      auto &type = types_[entry_type_];
+      type.insert(anchor_, name);
+    }
+
+    void add_argument_type(std::string name) {
+      if (!template_mapping_.count(name)) {
+        template_mapping_[name] = template_args_.size();
+        template_args_.push_back("T" + std::to_string(template_args_.size()));
+      }
+      const auto &template_name = template_args_[template_mapping_[name]];
+      auto &type = types_[entry_type_];
+      type.insert(anchor_, template_name);
+    }
+
+    void add_template_type(std::string name) {
+      auto &arg_type = types_[entry_type_];
+      arg_type.insert(anchor_, name + "<>");
+      anchor_ += name.size() + 1;
+    }
+
+    const std::vector<std::string> &
+    types() const {
+      return types_;
+    }
+
+    const std::vector<std::string> &
+    template_args() const {
+      return template_args_;
+    }
+
+  private:
+    std::string name_;
+
+    std::size_t entry_type_;
+
+    // the first type is the result type
+    std::vector<std::string> types_;
+    std::size_t anchor_;
+
+    std::map<std::string, std::size_t> template_mapping_;
+    std::vector<std::string> template_args_;
+};
+}
