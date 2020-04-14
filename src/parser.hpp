f67766a550fe6bc04426838867ff9dfffddb64c4
mu001999 <mu001999@outlook.com>
2020-04-14 14:43:20 +0800

    update

--- src/parser.hpp
+++ src/parser.hpp
@@ -9,6 +9,8 @@
 
 namespace hol2cpp
 {
+using parsec::Parsec;
+
 class Parser
 {
   public:
@@ -23,28 +25,28 @@ class Parser
 #ifndef _DEBUG
   private:
 #endif
-    parsec::Parsec<char> digit_, alpha_, blank_, blanks_;
+    Parsec<char> digit_, alpha_, blank_, blanks_;
 
-    parsec::Parsec<std::string> identifier_;
-    parsec::Parsec<std::string> func_decl_name_;
+    Parsec<std::string> identifier_;
+    Parsec<std::string> func_decl_name_;
 
-    parsec::Parsec<Ptr<Type>>               type_term_;
-    parsec::Parsec<std::vector<Ptr<Type>>>  func_type_tail_;
-    parsec::Parsec<Ptr<FuncType>>           func_type_;
-    parsec::Parsec<Ptr<Type>>               type_;
-    parsec::Parsec<Ptr<FuncType>>           func_decl_type_;
+    Parsec<Ptr<Type>>                   type_term_;
+    Parsec<std::vector<Ptr<Type>>>      func_type_tail_;
+    Parsec<Ptr<FuncType>>               func_type_;
+    Parsec<Ptr<Type>>                   type_;
+    Parsec<Ptr<FuncType>>               func_decl_type_;
 
-    parsec::Parsec<Ptr<Expr>> miniterm_;
-    parsec::Parsec<std::vector<Ptr<Expr>>>  miniterms_;
-    parsec::Parsec<Ptr<ConsExpr>>           cons_term_;
-    parsec::Parsec<Ptr<VarExpr>>            var_term_;
-    parsec::Parsec<Ptr<Expr>>               term_;
-    parsec::Parsec<Ptr<Expr>>               expr_;
+    Parsec<Ptr<Expr>> miniterm_;
+    Parsec<std::vector<Ptr<Expr>>>      miniterms_;
+    Parsec<Ptr<ConsExpr>>               cons_term_;
+    Parsec<Ptr<VarExpr>>                var_term_;
+    Parsec<Ptr<Expr>>                   term_;
+    Parsec<Ptr<Expr>>                   expr_;
 
-    parsec::Parsec<Ptr<Equation>>               func_decl_equation_;
-    parsec::Parsec<std::vector<Ptr<Equation>>>  func_decl_equations_;
+    Parsec<Ptr<Equation>>               func_decl_equation_;
+    Parsec<std::vector<Ptr<Equation>>>  func_decl_equations_;
 
-    parsec::Parsec<Ptr<FuncDecl>>               func_decl_;
-    parsec::Parsec<std::vector<Ptr<FuncDecl>>>  func_decls_;
+    Parsec<Ptr<FuncDecl>>               func_decl_;
+    Parsec<std::vector<Ptr<FuncDecl>>>  func_decls_;
 };
 } // namespace hol2cpp
