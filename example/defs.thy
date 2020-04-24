e609d93b80ded97e76f104ac62af1b1a1b836b99
mu001999 <mu001999@outlook.com>
2020-04-24 21:32:38 +0800

    add example source

---
+++ example/defs.thy
@@ -0,0 +1,15 @@
+fun add :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+  "add 0 n = n" |
+  "add (Suc m) n = Suc (add m n)"
+
+fun app :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+  "app Nil ys = ys" |
+  "app (Cons x xs) ys = Cons x (app xs ys)"
+
+fun rev :: "'a list \<Rightarrow> 'a list" where
+  "rev Nil = Nil" |
+  "rev (Cons x xs) = app (rev xs) (Cons x Nil)"
+
+fun rev2 :: "'a list \<Rightarrow> 'a list" where
+  "rev2 Nil = Nil" |
+  "rev2 (x # xs) = (rev xs) @ (x # Nil)"
