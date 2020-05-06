1d73de156648edd6b70e6865787ce0fb213441c6
mu001999 <mu001999@outlook.com>
2020-05-07 07:37:26 +0800

    add comments

--- src/code.hpp
+++ src/code.hpp
@@ -26,32 +26,66 @@ class Code
     void
     entry_func();
 
+    /**
+     * return the current entity to build
+    */
     FuncEntity
     &current_entity();
 
+    /**
+     * generate code and output by the out_ member
+    */
     void
     generate();
 
+    /**
+     * define or get the indent size
+     *  4 by default
+    */
     static
     std::size_t
     &indent_size();
 
+    /**
+     * add header file
+    */
     void
     add_header(const std::string &header);
 
   private:
+    /**
+     * generate code for each function declaration
+    */
     void
     generate_single(FuncEntity &entity);
+    /**
+     * generate code for type determined function
+    */
     void
     generate_normal(FuncEntity &entity);
+    /**
+     * generate code for template function
+     * add the template declaration before function declaration
+     *  such as template<typename T0...>
+     * then call method generate_normal to generate the rest code
+    */
     void
     generate_template(FuncEntity &entity);
 
+    /**
+     * start a new line with the indent
+    */
     std::ostream
     &new_line();
 
+    /**
+     * add the indent's length by indent size
+    */
     void
     add_indent();
+    /**
+     * sub the indent's length by indent size
+    */
     void
     sub_indent();
 
