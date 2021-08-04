3b033a09d44ab79abdce36417e4b97a2994ae88b
mu001999 <mu001999@outlook.com>
2021-08-04 15:10:38 +0800

    Update

--- example/demo.thy
+++ example/demo.thy
@@ -26,6 +26,16 @@ fun rev3 :: "'a list \<Rightarrow> 'a list" where
   "rev3 [] = []" |
   "rev3 (x # xs) = (rev3 xs) @ [x]"
 
+fun merge :: "nat list \<Rightarrow> nat list \<Rightarrow> nat list" where
+  "merge xs [] = xs" |
+  "merge [] ys = ys" |
+  "merge (x # xs) (y # ys) = If (x \<le> y) (x # (merge xs (y # ys))) (y # (merge (x # xs) ys))"
+
+fun merge_sort :: "nat list \<Rightarrow> nat list" where
+  "merge_sort [] = []" |
+  "merge_sort [x] = [x]" |
+  "merge_sort xs = merge (merge_sort (take ((length xs) div (nat 2)) xs)) (merge_sort (drop ((length xs) div (nat 2)) xs))"
+
 export_code rev3 in SML module_name demo
 
 end
