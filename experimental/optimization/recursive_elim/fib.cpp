f3c0f6747bbe2d452f3d014d7e52682c63f9f043
Mu001999 <mu001999@outlook.com>
2022-03-02 19:07:10 +0800

    Update

---
+++ experimental/optimization/recursive_elim/fib.cpp
@@ -0,0 +1,58 @@
+#include <utility>
+#include <stack>
+#include <tuple>
+
+std::uint64_t fib(std::uint64_t arg1) {
+    // fib 0 = 1
+    if (arg1 == 0) {
+        return 1;
+    }
+
+    // fib (Suc 0) = 1
+    if (arg1 != 0) {
+        if (arg1 - 1 == 0) {
+            return 1;
+        }
+    }
+
+    // fib n = (fib (n - 1)) + (fib (n - 2))
+    return fib(arg1 - 1) + fib(arg1 - 2);
+}
+
+std::uint64_t fib2(std::uint64_t arg1) {
+    std::stack<std::tuple<std::uint64_t>> args;
+    args.push(std::make_tuple(arg1));
+
+    std::stack<std::uint64_t> retvals;
+
+    std::stack<std::uint64_t> actions;
+    actions.push(0);
+
+    while (true) {
+        auto action = actions.top();
+        actions.pop();
+
+        switch (action) {
+            case 0:
+                auto [arg1] = args.top();
+                args.pop();
+
+                if (arg1 == 0) {
+                    retvals.push(1);
+                }
+
+                if (arg1 != 0) {
+                    if (arg1 - 1 == 0) {
+                        retvals.push(1);
+                    }
+                }
+
+
+
+            case 1:
+        }
+    }
+
+    // fib n = (fib (n - 1)) + (fib (n - 2))
+    return fib2(arg1 - 1) + fib2(arg1 - 2);
+}
