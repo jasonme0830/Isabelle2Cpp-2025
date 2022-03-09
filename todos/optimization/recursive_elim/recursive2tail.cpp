90b0ba36633fc45e92aa12c2b5f393845187c6d0
Mu001999 <mu001999@outlook.com>
2022-03-09 16:26:00 +0800

    Three steps

--- todos/optimization/recursive_elim/recursive2tail.cpp
+++ todos/optimization/recursive_elim/recursive2tail.cpp
@@ -1,6 +1,4 @@
 #include <utility>
-#include <stack>
-#include <tuple>
 
 std::uint64_t factorial(const std::uint64_t &arg1) {
     // factorial 0 = 1
@@ -89,6 +87,7 @@ std::uint64_t mult_termial2(std::uint64_t arg1, std::uint64_t acc = 0) {
 
     return mult_termial2(arg1 - 2, arg1 + acc);
 }
+
 /**
  * above cases need finding identity-element,
  * but the following cases need human reasoning
@@ -125,59 +124,4 @@ std::uint64_t factorial_termial2(std::uint64_t arg1, std::uint64_t acc1 = 0, std
     }
 
     return arg1 * factorial_termial2(arg1 - 2);
-}
-
-std::uint64_t fib(std::uint64_t arg1) {
-    // fib 0 = 1
-    if (arg1 == 0) {
-        return 1;
-    }
-
-    // fib (Suc 0) = 1
-    if (arg1 != 0) {
-        if (arg1 - 1 == 0) {
-            return 1;
-        }
-    }
-
-    // fib n = (fib (n - 1)) + (fib (n - 2))
-    return fib(arg1 - 1) + fib(arg1 - 2);
-}
-
-std::uint64_t fib2(std::uint64_t arg1) {
-    std::stack<std::tuple<std::uint64_t>> args;
-    args.push(std::make_tuple(arg1));
-
-    std::stack<std::uint64_t> retvals;
-
-    std::stack<std::uint64_t> actions;
-    actions.push(0);
-
-    while (true) {
-        auto action = actions.top();
-        actions.pop();
-
-        switch (action) {
-            case 0:
-                auto [arg1] = args.top();
-                args.pop();
-
-                if (arg1 == 0) {
-                    retvals.push(1);
-                }
-
-                if (arg1 != 0) {
-                    if (arg1 - 1 == 0) {
-                        retvals.push(1);
-                    }
-                }
-
-
-
-            case 1:
-        }
-    }
-
-    // fib n = (fib (n - 1)) + (fib (n - 2))
-    return fib2(arg1 - 1) + fib2(arg1 - 2);
 }
