83da5a9d1019dd41e48d5c03c34ea9da3550023a
Mu001999 <mu001999@outlook.com>
2022-03-10 15:01:54 +0800

    Update the demo

--- todos/optimization/recursive_elim/tail2loop.cpp
+++ todos/optimization/recursive_elim/tail2loop.cpp
@@ -1,11 +1,7 @@
 #include <utility>
+#include <tuple>
 
-// same as the function in recursive2tail.cpp, which has parameters with default values
-std::uint64_t factorial_termial2(std::uint64_t arg1) {
-    return factorial_termial2_impl(arg1, 0, 1);
-}
-
-std::uint64_t factorial_termial2_impl(std::uint64_t arg1, std::uint64_t acc1, std::uint64_t acc2) {
+std::uint64_t factorial_termial2(std::uint64_t arg1, std::uint64_t acc1 = 0, std::uint64_t acc2 = 1) {
     if (arg1 <= 1) {
         return 1 + acc1;
     } else if (arg1 == 2) {
@@ -13,31 +9,34 @@ std::uint64_t factorial_termial2_impl(std::uint64_t arg1, std::uint64_t acc1, st
     }
 
     if (arg1 % 2 == 1) {
-        return factorial_termial2_impl(arg1 - 2, arg1 + acc1, acc2);
+        return factorial_termial2(arg1 - 2, arg1 + acc1, acc2);
     }
 
-    return factorial_termial2_impl(arg1 - 2, acc1, arg1 * acc2);
+    return factorial_termial2(arg1 - 2, acc1, arg1 * acc2);
 }
 
-// convert to loop
-std::uint64_t factorial_termial2_impl2(std::uint64_t arg1, std::uint64_t acc1, std::uint64_t acc2) {
+// tail to loop
+std::uint64_t factorial_termial3(std::uint64_t arg1) {
+    std::uint64_t acc1 = 0;
+    std::uint64_t acc2 = 1;
+
     while (true) {
         if (arg1 <= 1) {
             return 1 + acc1;
         } else if (arg1 == 2) {
             return 2 * acc2;
         }
-    }
 
-    if (arg1 <= 1) {
-        return 1 + acc1;
-    } else if (arg1 == 2) {
-        return 2 * acc2;
-    }
+        if (arg1 % 2 == 1) {
+            std::tie(arg1, acc1, acc2) = std::forward_as_tuple(
+                arg1 - 2, arg1 + acc1, acc2
+            );
+            continue;
+        }
 
-    if (arg1 % 2 == 1) {
-        return factorial_termial2_impl2(arg1 - 2, arg1 + acc1, acc2);
+        std::tie(arg1, acc1, acc2) = std::forward_as_tuple(
+            arg1 - 2, acc1, arg1 * acc2
+        );
+        continue;
     }
-
-    return factorial_termial2_impl2(arg1 - 2, acc1, arg1 * acc2);
 }
