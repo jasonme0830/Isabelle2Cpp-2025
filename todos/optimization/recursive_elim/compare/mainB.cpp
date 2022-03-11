488af9233922c44f55101f7ca388103faf9c3a3b
Mu001999 <mu001999@outlook.com>
2022-03-11 14:50:22 +0800

    Use one stack for one variable

--- todos/optimization/recursive_elim/compare/mainB.cpp
+++ todos/optimization/recursive_elim/compare/mainB.cpp
@@ -7,9 +7,7 @@ std::uint64_t ft4_loop(std::uint64_t arg1) {
     states.push(0);
 
     std::stack<std::uint64_t> rets;
-
-    std::stack<std::tuple<std::uint64_t>> args1;
-    std::stack<std::tuple<std::uint64_t>> args2;
+    std::stack<std::uint64_t> arg1s;
 
     while (!states.empty()) {
         auto state = states.top(); states.pop();
@@ -24,7 +22,7 @@ case 0:
         }
 
         if (arg1 % 2 == 1) {
-            args1.push(std::make_tuple(arg1)); // store the variable
+            arg1s.push(arg1); // store the variable
             states.push(1);
 
             std::tie(arg1) = std::forward_as_tuple(arg1 - 2);
@@ -32,7 +30,7 @@ case 0:
             continue;
         }
 
-        args2.push(std::make_tuple(arg1));
+        arg1s.push(arg1);
         states.push(2);
 
         std::tie(arg1) = std::forward_as_tuple(arg1 - 2);
@@ -40,13 +38,13 @@ case 0:
         continue;
 
 case 1: {
-        auto [arg1] = args1.top(); args1.pop();
+        auto arg1 = arg1s.top(); arg1s.pop();
         auto ret1 = rets.top(); rets.pop();
         rets.push(arg1 + ret1);
         continue;
 }
 case 2: {
-        auto [arg1] = args2.top(); args2.pop();
+        auto arg1 = arg1s.top(); arg1s.pop();
         auto ret1 = rets.top(); rets.pop();
         rets.push(arg1 * ret1);
         continue;
