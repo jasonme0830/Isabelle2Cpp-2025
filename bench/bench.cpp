171f315ba4bdd716496b5ba1b6f73702eccdf7ea
Mu001999 <mu001999@outlook.com>
2022-03-22 15:25:27 +0800

    Remove the generated files

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -1,9 +1,10 @@
 #include <benchmark/benchmark.h>
 #include <cassert>
-#include "move-list/merge_sort.enable.hpp"
-#include "move-list/merge_sort.disable.hpp"
+#include "merge_sort/merge_sort.hpp"
+#include "merge_sort/merge_sort.ml.hpp"
+#include "merge_sort/merge_sort.ml.rc.hpp"
 
-static void bench_disable_move_list(benchmark::State &state) {
+static void bench_merge_sort(benchmark::State &state) {
     auto length = state.range(0);
 
     std::list<std::uint64_t> list;
@@ -14,7 +15,7 @@ static void bench_disable_move_list(benchmark::State &state) {
     sorted_list.sort();
 
     for (auto _ : state) {
-        auto nlist = merge_sort1(list);
+        auto nlist = merge_sort(list);
 
         state.PauseTiming();
         assert(sorted_list == nlist);
@@ -22,7 +23,7 @@ static void bench_disable_move_list(benchmark::State &state) {
     }
 }
 
-static void bench_enable_move_list(benchmark::State &state) {
+static void bench_merge_sort_ml(benchmark::State &state) {
     auto length = state.range(0);
 
     std::list<std::uint64_t> list;
@@ -33,7 +34,7 @@ static void bench_enable_move_list(benchmark::State &state) {
     sorted_list.sort();
 
     for (auto _ : state) {
-        auto nlist = merge_sort2(list);
+        auto nlist = merge_sort_ml(list);
 
         state.PauseTiming();
         assert(sorted_list == nlist);
@@ -41,6 +42,26 @@ static void bench_enable_move_list(benchmark::State &state) {
     }
 }
 
-BENCHMARK(bench_disable_move_list)->RangeMultiplier(2)->Range(256, 16384);
-BENCHMARK(bench_enable_move_list)->RangeMultiplier(2)->Range(256, 16384);
+static void bench_merge_sort_ml_rc(benchmark::State &state) {
+    auto length = state.range(0);
+
+    std::list<std::uint64_t> list;
+    for (std::uint64_t i = length; i > 0; --i) {
+        list.push_back(i);
+    }
+    auto sorted_list = list;
+    sorted_list.sort();
+
+    for (auto _ : state) {
+        auto nlist = merge_sort_ml_rc(list);
+
+        state.PauseTiming();
+        assert(sorted_list == nlist);
+        state.ResumeTiming();
+    }
+}
+
+BENCHMARK(bench_merge_sort)->RangeMultiplier(2)->Range(256, 16384);
+BENCHMARK(bench_merge_sort_ml)->RangeMultiplier(2)->Range(256, 16384);
+BENCHMARK(bench_merge_sort_ml_rc)->RangeMultiplier(2)->Range(256, 16384);
 BENCHMARK_MAIN();
