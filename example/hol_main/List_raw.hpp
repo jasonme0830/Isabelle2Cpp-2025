2247ab1252a14c4caea2efce37f2bc4e2a7b33cb
Mu001999 <mu001999@outlook.com>
2022-03-22 10:53:17 +0800

    Apply the new conversion

--- example/hol_main/List_raw.hpp
+++ example/hol_main/List_raw.hpp
@@ -17,7 +17,7 @@ T1 last(std::list<T1> arg1) {
         if (xs.empty()) {
             temp0 = x;
         } else {
-            temp0 = last(xs);
+            temp0 = last(std::move(xs));
         }
         return temp0;
     } else { // auto-generated for -Wreturn-type
@@ -41,7 +41,7 @@ std::list<T1> butlast(std::list<T1> arg1) {
         if (xs.empty()) {
             temp0 = std::list<T1>();
         } else {
-            auto temp1 = butlast(xs);
+            auto temp1 = butlast(std::move(xs));
             temp1.push_front(x);
             temp0 = std::move(temp1);
         }
@@ -63,7 +63,7 @@ std::list<T1> rev(std::list<T1> arg1) {
         auto x = arg1.front();
         arg1.erase(arg1.begin(), std::next(arg1.begin(), 1));
         auto xs = std::move(arg1);
-        auto temp0 = rev(xs);
+        auto temp0 = rev(std::move(xs));
         auto temp1 = std::list<T1>{x};
         temp0.splice(temp0.end(), temp1);
         return temp0;
@@ -86,11 +86,11 @@ std::list<T1> filter(const std::function<bool(const T1 &)> &arg1, std::list<T1> 
         auto xs = std::move(arg2);
         std::list<T1> temp0;
         if (P(x)) {
-            auto temp1 = filter(arg1, xs);
+            auto temp1 = filter(arg1, std::move(xs));
             temp1.push_front(x);
             temp0 = std::move(temp1);
         } else {
-            temp0 = filter(arg1, xs);
+            temp0 = filter(arg1, std::move(xs));
         }
         return temp0;
     } else { // auto-generated for -Wreturn-type
@@ -110,7 +110,7 @@ T1 foldl(const std::function<T1(const T1 &, const T2 &)> &arg1, const T1 &arg2, 
         auto x = arg3.front();
         arg3.erase(arg3.begin(), std::next(arg3.begin(), 1));
         auto xs = std::move(arg3);
-        return foldl(arg1, f(arg2, x), xs);
+        return foldl(arg1, f(arg2, x), std::move(xs));
     } else { // auto-generated for -Wreturn-type
         std::abort();
     }
@@ -128,8 +128,8 @@ std::list<T1> concat(std::list<std::list<T1>> arg1) {
         auto x = arg1.front();
         arg1.erase(arg1.begin(), std::next(arg1.begin(), 1));
         auto xs = std::move(arg1);
-        auto temp0 = x;
-        auto temp1 = concat(xs);
+        auto temp0 = std::move(x);
+        auto temp1 = concat(std::move(xs));
         temp0.splice(temp0.end(), temp1);
         return temp0;
     } else { // auto-generated for -Wreturn-type
@@ -154,7 +154,7 @@ T1 nth(std::list<T1> arg1, const std::uint64_t &arg2) {
 
             // Suc k \<Rightarrow> xs ! k
             if (temp1 != 0) {
-                return nth(xs, k);
+                return nth(std::move(xs), k);
             } else { // auto-generated for -Wreturn-type
                 std::abort();
             }
@@ -182,14 +182,14 @@ std::list<T1> list_update(std::list<T1> arg1, const std::uint64_t &arg2, const T
 
             // 0 \<Rightarrow> v # xs
             if (temp1 == 0) {
-                auto temp2 = xs;
+                auto temp2 = std::move(xs);
                 temp2.push_front(arg3);
                 return std::move(temp2);
             }
 
             // Suc j \<Rightarrow> x # list_update xs j v
             if (temp1 != 0) {
-                auto temp3 = list_update(xs, j, arg3);
+                auto temp3 = list_update(std::move(xs), j, arg3);
                 temp3.push_front(x);
                 return std::move(temp3);
             } else { // auto-generated for -Wreturn-type
@@ -216,7 +216,7 @@ std::list<T1> takeWhile(const std::function<bool(const T1 &)> &arg1, std::list<T
         auto xs = std::move(arg2);
         std::list<T1> temp0;
         if (P(x)) {
-            auto temp1 = takeWhile(arg1, xs);
+            auto temp1 = takeWhile(arg1, std::move(xs));
             temp1.push_front(x);
             temp0 = std::move(temp1);
         } else {
@@ -242,9 +242,9 @@ std::list<T1> dropWhile(const std::function<bool(const T1 &)> &arg1, std::list<T
         auto xs = std::move(arg2);
         std::list<T1> temp0;
         if (P(x)) {
-            temp0 = dropWhile(arg1, xs);
+            temp0 = dropWhile(arg1, std::move(xs));
         } else {
-            auto temp1 = xs;
+            auto temp1 = std::move(xs);
             temp1.push_front(x);
             temp0 = std::move(temp1);
         }
@@ -276,7 +276,7 @@ std::list<std::pair<T1, T2>> zip(std::list<T1> arg1, std::list<T2> arg2) {
 
             // z # zs \<Rightarrow> (z, y) # zip zs ys
             if (temp1.size() >= 1) {
-                auto temp2 = zip(zs, ys);
+                auto temp2 = zip(std::move(zs), std::move(ys));
                 temp2.push_front(std::make_pair(z, y));
                 return std::move(temp2);
             } else { // auto-generated for -Wreturn-type
@@ -294,9 +294,9 @@ std::list<T1> insert(const T1 &arg1, std::list<T1> arg2) {
     // insert x xs = (if x \<in> set xs then xs else x # xs)
     std::list<T1> temp0;
     if (set(arg2).count(arg1)) {
-        temp0 = arg2;
+        temp0 = std::move(arg2);
     } else {
-        auto temp1 = arg2;
+        auto temp1 = std::move(arg2);
         temp1.push_front(arg1);
         temp0 = std::move(temp1);
     }
@@ -319,7 +319,7 @@ std::optional<T1> find(const std::function<bool(const T1 &)> &arg1, std::list<T1
         if (P(x)) {
             temp0 = std::make_optional<T1>(x);
         } else {
-            temp0 = find(arg1, xs);
+            temp0 = find(arg1, std::move(xs));
         }
         return temp0;
     } else { // auto-generated for -Wreturn-type
@@ -341,9 +341,9 @@ std::uint64_t count_list(std::list<T1> arg1, const T1 &arg2) {
         auto xs = std::move(arg1);
         std::uint64_t temp0;
         if (x == arg2) {
-            temp0 = count_list(xs, arg2) + 1;
+            temp0 = count_list(std::move(xs), arg2) + 1;
         } else {
-            temp0 = count_list(xs, arg2);
+            temp0 = count_list(std::move(xs), arg2);
         }
         return temp0;
     } else { // auto-generated for -Wreturn-type
@@ -365,9 +365,9 @@ std::list<T1> remove1(const T1 &arg1, std::list<T1> arg2) {
         auto xs = std::move(arg2);
         std::list<T1> temp0;
         if (arg1 == y) {
-            temp0 = xs;
+            temp0 = std::move(xs);
         } else {
-            auto temp1 = remove1(arg1, xs);
+            auto temp1 = remove1(arg1, std::move(xs));
             temp1.push_front(y);
             temp0 = std::move(temp1);
         }
@@ -391,9 +391,9 @@ std::list<T1> removeAll(const T1 &arg1, std::list<T1> arg2) {
         auto xs = std::move(arg2);
         std::list<T1> temp0;
         if (arg1 == y) {
-            temp0 = removeAll(arg1, xs);
+            temp0 = removeAll(arg1, std::move(xs));
         } else {
-            auto temp1 = removeAll(arg1, xs);
+            auto temp1 = removeAll(arg1, std::move(xs));
             temp1.push_front(y);
             temp0 = std::move(temp1);
         }
@@ -415,7 +415,7 @@ bool distinct(std::list<T1> arg1) {
         auto x = arg1.front();
         arg1.erase(arg1.begin(), std::next(arg1.begin(), 1));
         auto xs = std::move(arg1);
-        return !set(xs).count(x) && distinct(xs);
+        return !set(xs).count(x) && distinct(std::move(xs));
     } else { // auto-generated for -Wreturn-type
         std::abort();
     }
@@ -435,9 +435,9 @@ std::list<T1> remdups(std::list<T1> arg1) {
         auto xs = std::move(arg1);
         std::list<T1> temp0;
         if (set(xs).count(x)) {
-            temp0 = remdups(xs);
+            temp0 = remdups(std::move(xs));
         } else {
-            auto temp1 = remdups(xs);
+            auto temp1 = remdups(std::move(xs));
             temp1.push_front(x);
             temp0 = std::move(temp1);
         }
@@ -468,11 +468,11 @@ std::list<T1> remdups_adj(std::list<T1> arg1) {
         auto xs = std::move(arg1);
         std::list<T1> temp0;
         if (x == y) {
-            auto temp1 = xs;
+            auto temp1 = std::move(xs);
             temp1.push_front(x);
             temp0 = remdups_adj(std::move(temp1));
         } else {
-            auto temp2 = xs;
+            auto temp2 = std::move(xs);
             temp2.push_front(y);
             auto temp3 = remdups_adj(std::move(temp2));
             temp3.push_front(x);
@@ -496,7 +496,7 @@ std::list<T1> replicate(const std::uint64_t &arg1, const T1 &arg2) {
         auto n = arg1 - 1;
         auto temp0 = replicate(n, arg2);
         temp0.push_front(arg2);
-        return std::move(temp0);
+        return temp0;
     } else { // auto-generated for -Wreturn-type
         std::abort();
     }
@@ -507,11 +507,11 @@ std::list<std::pair<std::uint64_t, T1>> enumerate(const std::uint64_t &arg1, std
     // enumerate n xs = zip [n..<n + length xs] xs
     auto temp0 = arg1;
     auto temp1 = arg1 + arg2.size();
-    std::list<decltype(temp0)> temp2;
+    std::list<T1> temp2;
     for (auto temp3 = temp0; temp3 < temp1; ++temp3) {
         temp2.push_back(temp3);
     }
-    return zip(temp2, arg2);
+    return zip(temp2, std::move(arg2));
 }
 
 template<typename T1>
@@ -526,13 +526,28 @@ std::list<T1> rotate1(std::list<T1> arg1) {
         auto x = arg1.front();
         arg1.erase(arg1.begin(), std::next(arg1.begin(), 1));
         auto xs = std::move(arg1);
-        auto temp0 = xs;
+        auto temp0 = std::move(xs);
         auto temp1 = std::list<T1>{x};
         temp0.splice(temp0.end(), temp1);
         return temp0;
     } else { // auto-generated for -Wreturn-type
         std::abort();
     }
+}
+
+template<typename T1>
+std::list<T1> nths(std::list<T1> arg1, const std::set<std::uint64_t> &arg2) {
+    // nths xs A = map fst (filter (\<lambda>p. snd p \<in> A) (zip xs [0..<size xs]))
+    std::function temp0 = [=] (const T1 &p) {
+        return arg2.count(snd(p));
+    };
+    auto temp1 = 0;
+    auto temp2 = size(arg1);
+    std::list<T2> temp3;
+    for (auto temp4 = temp1; temp4 < temp2; ++temp4) {
+        temp3.push_back(temp4);
+    }
+    return map(fst, filter(temp0, zip(arg1, temp3)));
 }
 
 template<typename T1>
@@ -547,9 +562,9 @@ std::list<T1> splice(std::list<T1> arg1, std::list<T1> arg2) {
         auto x = arg1.front();
         arg1.erase(arg1.begin(), std::next(arg1.begin(), 1));
         auto xs = std::move(arg1);
-        auto temp0 = splice(arg2, xs);
+        auto temp0 = splice(std::move(arg2), std::move(xs));
         temp0.push_front(x);
-        return std::move(temp0);
+        return temp0;
     } else { // auto-generated for -Wreturn-type
         std::abort();
     }
@@ -570,7 +585,7 @@ T1 min_list(std::list<T1> arg1) {
             }
 
             // _ \<Rightarrow> min x (min_list xs)
-            return min(x, min_list(xs));
+            return min(x, min_list(std::move(xs)));
         })();
         return temp0;
     } else { // auto-generated for -Wreturn-type
@@ -592,7 +607,7 @@ T1 arg_min_list(const std::function<T2(const T1 &)> &arg1, std::list<T1> arg2) {
         auto y = *std::next(arg2.begin(), 1);
         arg2.erase(arg2.begin(), std::next(arg2.begin(), 2));
         auto zs = std::move(arg2);
-        auto temp1 = zs;
+        auto temp1 = std::move(zs);
         temp1.push_front(y);
         auto temp0 = arg_min_list(arg1, std::move(temp1));
         T1 temp2;
@@ -621,13 +636,13 @@ std::list<T1> insort_key(const std::function<T2(const T1 &)> &arg1, const T1 &ar
         auto ys = std::move(arg3);
         std::list<T1> temp0;
         if (f(arg2) <= f(y)) {
-            auto temp1 = ys;
+            auto temp1 = std::move(ys);
             temp1.push_front(y);
             auto temp2 = std::move(temp1);
             temp2.push_front(arg2);
             temp0 = std::move(temp2);
         } else {
-            auto temp3 = insort_key(arg1, arg2, ys);
+            auto temp3 = insort_key(arg1, arg2, std::move(ys));
             temp3.push_front(y);
             temp0 = std::move(temp3);
         }
@@ -649,16 +664,16 @@ std::pair<std::list<T1>, std::list<T1>> partition(const std::function<bool(const
         auto x = arg2.front();
         arg2.erase(arg2.begin(), std::next(arg2.begin(), 1));
         auto xs = std::move(arg2);
-        auto temp0 = partition(arg1, xs);
+        auto temp0 = partition(arg1, std::move(xs));
         std::pair<std::list<T1>, std::list<T1>> temp1;
         if (P(x)) {
-            auto temp2 = yes;
+            auto temp2 = std::move(yes);
             temp2.push_front(x);
-            temp1 = std::make_pair(std::move(temp2), no);
+            temp1 = std::make_pair(std::move(temp2), std::move(no));
         } else {
-            auto temp3 = no;
+            auto temp3 = std::move(no);
             temp3.push_front(x);
-            temp1 = std::make_pair(yes, std::move(temp3));
+            temp1 = std::make_pair(std::move(yes), std::move(temp3));
         }
         return temp1;
     } else { // auto-generated for -Wreturn-type
@@ -680,7 +695,7 @@ std::set<std::list<T1>> listset(std::list<std::set<T1>> arg1) {
         auto A = arg1.front();
         arg1.erase(arg1.begin(), std::next(arg1.begin(), 1));
         auto As = std::move(arg1);
-        return set_Cons(A, listset(As));
+        return set_Cons(A, listset(std::move(As)));
     } else { // auto-generated for -Wreturn-type
         std::abort();
     }
@@ -704,9 +719,9 @@ std::list<T2> map_tailrec_rev(const std::function<T2(const T1 &)> &arg1, std::li
         auto a = arg2.front();
         arg2.erase(arg2.begin(), std::next(arg2.begin(), 1));
         auto as = std::move(arg2);
-        auto temp0 = arg3;
+        auto temp0 = std::move(arg3);
         temp0.push_front(f(a));
-        return map_tailrec_rev(arg1, as, std::move(temp0));
+        return map_tailrec_rev(arg1, std::move(as), std::move(temp0));
     } else { // auto-generated for -Wreturn-type
         std::abort();
     }
@@ -715,7 +730,7 @@ std::list<T2> map_tailrec_rev(const std::function<T2(const T1 &)> &arg1, std::li
 template<typename T1, typename T2>
 std::list<T2> map_tailrec(const std::function<T2(const T1 &)> &arg1, std::list<T1> arg2) {
     // map_tailrec f as = rev (map_tailrec_rev f as [])
-    return rev(map_tailrec_rev(arg1, arg2, {}));
+    return rev(map_tailrec_rev(arg1, std::move(arg2), std::list<T2>()));
 }
 
 template<typename T1>
@@ -748,3 +763,4 @@ std::uint64_t gen_length(const std::uint64_t &arg1, std::list<T1> arg2) {
     return arg1 + arg2.size();
 }
 
+// generated by HOL2Cpp
