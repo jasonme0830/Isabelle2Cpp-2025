77d53537e9590f57b0f6fbaa9d6912116812e520
Mu001999 <mu001999@outlook.com>
2022-03-28 17:07:26 +0800

    Update the benchmark source

---
+++ bench/binary_search/binary_search.ml.rc.thy
@@ -0,0 +1,10 @@
+theory binary_search_ml_rc
+imports Main
+begin
+
+fun bs_ml_rc :: "nat \<Rightarrow> nat list \<Rightarrow> nat option" where
+  "bs_ml_rc x [] = None" |
+  "bs_ml_rc x [y] = If (x = y) (Some x) None" |
+  "bs_ml_rc x ys = (let m = (length ys) div 2 in let y = ys ! m in If (y = x) (Some x) (If (y < x) (bs_ml_rc x (drop (m + 1) ys)) (bs_ml_rc x (take m ys))))"
+
+end
