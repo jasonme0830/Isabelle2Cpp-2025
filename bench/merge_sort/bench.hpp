a78b08ab263c502be5e817d28659359c7d5b8e5c
Mu00 <mu001999@outlook.com>
2023-02-25 10:24:31 +0800

    添加与人工编写版本的基准测试 (#53)

--- bench/merge_sort/bench.hpp
+++ bench/merge_sort/bench.hpp
@@ -5,9 +5,48 @@
 #include "merge_sort.ud.hpp"
 #include "merge_sort.ml.rc.hpp"
 #include "merge_sort.ml.ud.hpp"
+#include "merge_sort_raw.hpp"
 
 #include <cassert>
 #include <benchmark/benchmark.h>
+
+static void bench_merge_sort_raw(benchmark::State &state) {
+    auto length = state.range(0);
+
+    std::list<std::uint64_t> list;
+    for (std::uint64_t i = length; i > 0; --i) {
+        list.push_back(i);
+    }
+    auto sorted_list = list;
+    sorted_list.sort();
+
+    for (auto _ : state) {
+        auto nlist = merge_sort_r(list);
+
+        state.PauseTiming();
+        assert(sorted_list == nlist);
+        state.ResumeTiming();
+    }
+}
+
+static void bench_merge_sort_raw_ud(benchmark::State &state) {
+    auto length = state.range(0);
+
+    std::deque<std::uint64_t> list;
+    for (std::uint64_t i = length; i > 0; --i) {
+        list.push_back(i);
+    }
+    auto sorted_list = list;
+    std::sort(sorted_list.begin(), sorted_list.end());
+
+    for (auto _ : state) {
+        auto nlist = merge_sort_r_ud(list);
+
+        state.PauseTiming();
+        assert(sorted_list == nlist);
+        state.ResumeTiming();
+    }
+}
 
 static void bench_merge_sort(benchmark::State &state) {
     auto length = state.range(0);
@@ -104,6 +143,8 @@ static void bench_merge_sort_ml_ud(benchmark::State &state) {
     }
 }
 
+BENCHMARK(bench_merge_sort_raw)->RangeMultiplier(2)->Range(256, 16384);
+BENCHMARK(bench_merge_sort_raw_ud)->RangeMultiplier(2)->Range(256, 16384);
 BENCHMARK(bench_merge_sort)->RangeMultiplier(2)->Range(256, 16384);
 BENCHMARK(bench_merge_sort_ml)->RangeMultiplier(2)->Range(256, 16384);
 BENCHMARK(bench_merge_sort_ud)->RangeMultiplier(2)->Range(256, 16384);
