4e5d926f5ce266ee33b625a2a4a007c0e4482db2
jasonme0830 <2796931057@qq.com>
2024-07-10 14:10:06 +0800

    version one

--- example/baseCrule/rule_type.thy
+++ example/baseCrule/rule_type.thy
@@ -1,18 +1,18 @@
-theory rule_tree
+theory rule_type
   imports Main
 begin
 
 datatype 'a tree = Tip | Node "'a tree " 'a "'a tree " 
 
 
-fun AddListHead::"'a=>'a list=>'a list"where
+fun AddListHead::"'a =>'a list=>'a list"where
 "AddListHead a xs =a # xs "
 
-fun AddListTail::"'a=>'a list =>'a list"where
+fun AddListTail::"'a =>'a list =>'a list"where
 "AddListTail a [] =Cons a [] "|(*!*)
 "AddListTail a (x#xs) = x # (AddListTail a xs ) "
 
-fun AddListI::"nat=>'a=>'a list=>'a list"where
+fun AddListI::"nat =>'a =>'a list=>'a list"where
 "AddListI i a [] = Cons a [] "|
 "AddListI 0 a (x#xs) = AddListHead a (x#xs)"|
 "AddListI (Suc i) a (x#xs) = x # (AddListI i a xs) "
@@ -21,26 +21,26 @@ fun DelListHead::"'a list=>'a list "where
 "DelListHead (x#xs) = xs"|
 "DelListHead [] = [] "
 
-fun DelListTail::"'a list=>'a list "where
+fun DelListTail::"'a list =>'a list "where
 "DelListTail [] = []"|
 "DelListTail (Cons a []) = []"|
 "DelListTail (x#xs) =  Cons x (DelListTail xs) "
 
-fun DelListI::"nat=>'a list=>'a list "where
+fun DelListI::"nat =>'a list =>'a list "where
 "DelListI i [] = []"|
 "DelListI i (Cons a []) = []"|
 "DelListI 0 (x#xs) = DelListHead (x#xs) "|
 "DelListI (Suc i) (x#xs) = x # (DelListI i xs) "
 
-fun SearchList::"'a=>'a list=>bool "where
+fun SearchList::"'a =>'a list =>bool "where
 "SearchList a [] = False"|
 "SearchList a (x#xs) = (if a=x then True else (SearchList a xs))"
 
-fun Modify1::"'a=>'a=> 'a list =>'a list"where
+fun Modify1::"'a =>'a => 'a list =>'a list"where
 "Modify1 a b [] = [] "|
 "Modify1 a b (x#xs) = (if x=a then b#(Modify1 a b xs) else x#(Modify1 a b xs)) "
 
-fun Modify2::"nat=>'a=>'a list=>'a list "where
+fun Modify2::"nat =>'a =>'a list =>'a list "where
 "Modify2 n b [] = []"|
 "Modify2 0 b (x#xs) =(b#xs)"|
 "Modify2 (Suc n) b (x#xs) = (x#(Modify2 n b xs))"
@@ -49,11 +49,6 @@ fun ffilter::"('a=>bool) =>'a list=>'a list "where
 "ffilter f [] = [] "|
 "ffilter f(x#xs) = (if (f x) then (x#(ffilter f xs))else(ffilter f xs ))"
 
-fun Reverse2::"'a list=>'a list "where
-"Reverse2 [] = []"|
-"Reverse2 (Cons a []) =Cons a [] "|
-"Reverse2 (x#xs) = ( Reverse2 xs)@ (Cons x Nil) "
-
 fun app::  "'a list =>'a list => 'a list " where 
 "app Nil as =as"|
 "app(Cons a as )bs= Cons a (app as bs)"
@@ -61,6 +56,11 @@ fun app::  "'a list =>'a list => 'a list " where
 fun Reverse::  "'a list =>'a list " where
 "Reverse Nil =Nil"|
 "Reverse (Cons a as) = app(Reverse as)(Cons a Nil)"
+
+fun Reverse2::"'a list=>'a list "where
+"Reverse2 [] = []"|
+"Reverse2 (Cons a []) =Cons a [] "|
+"Reverse2 (x#xs) = ( Reverse2 xs)@ (Cons x Nil) "
 
 fun Insert::"'a::ord => 'a list => 'a list " where
 "Insert a [] =Cons a [] "|
@@ -72,6 +72,26 @@ fun InsertSortPart::"('a::ord)list => 'a list => 'a list "where
 
 fun InsertSort::"('a::ord)list=>'a list " where
 "InsertSort xs = InsertSortPart xs Nil"
+
+fun ssize :: "'a list => nat" where  
+  "ssize Nil = 0" |  
+  "ssize (Cons x Nil) = (Suc 0)" |
+  "ssize (Cons x xs) = Suc (ssize xs)"
+
+fun slength :: "'a list => nat" where  
+  "slength Nil = 0" |  
+  "slength (Cons x Nil) = (Suc 0)" |
+  "slength (Cons x xs) = (Suc (slength xs))"
+
+fun stake :: "nat => 'a list => 'a list" where  
+  "stake 0 xs = Nil" |  
+  "stake (Suc n) Nil = Nil" | 
+  "stake (Suc n) (Cons x xs) =(Cons x (stake n xs))"  
+
+fun sdrop :: "nat => 'a list => 'a list" where  
+  "sdrop 0 xs = xs" |  
+  "sdrop (Suc n) Nil = Nil" |  
+  "sdrop (Suc n) (Cons x xs) = (sdrop n xs)"
 
 fun Merge::"('a::ord)list => 'a list => 'a list "where
 "Merge [] xs=xs"|
@@ -105,43 +125,43 @@ fun fib :: "nat \<Rightarrow> nat" where
 
 
 
-fun searchtree1::"'a=>'a tree=>bool " where
-"searchtree1 a Tip=False "|
+fun searchtree1::"'a =>'a tree =>bool " where
+"searchtree1 a Tip = False "|
 "searchtree1 a (Node left x right) = (a= x \<or> (searchtree1 a left)\<or> (searchtree1 a right)) "
 
-fun searchtree2::"'a=>'a tree=>bool " where
-"searchtree2 a Tip=False "|
+fun searchtree2::"'a =>'a tree =>bool " where
+"searchtree2 a Tip = False "|
 "searchtree2 a (Node left x right) = ( (searchtree2 a left) \<or> (a=x) \<or>  (searchtree2 a right)) "
 
-fun searchtree3::"'a=>'a tree=>bool " where
-"searchtree3 a Tip=False "|
+fun searchtree3::"'a => 'a tree => bool " where
+"searchtree3 a Tip = False "|
 "searchtree3 a (Node left x right) = ( (searchtree3 a left)\<or> (searchtree3 a right) \<or> (a=x) ) "
 
-fun inserttree::"'a::ord=> 'a tree => 'a tree " where
-"inserttree a Tip= Node Tip a Tip"|
+fun inserttree::"'a::ord => 'a tree => 'a tree " where
+"inserttree a Tip = Node Tip a Tip"|
 "inserttree a (Node left x right) = (if a\<le>x then (Node (inserttree a left) x right) 
                                      else (Node left x (inserttree a right) ) )"
 
 fun transtolist:: "'a tree =>'a list " where
-"transtolist Tip=[] "|
+"transtolist Tip = [] "|
 (*"transtolist (Node Tip a Tip) = [a]  "|*)
 "transtolist (Node left a right) =( a # (transtolist left)@(transtolist right)) "
 
-fun rightest::"'a tree=>'a" where
+fun rightest::"'a tree => 'a" where
 "rightest (Node left x right) = (if right=Tip then x 
                                  else (rightest right)) "
 (*"rightest Tip=  "*)
 
-fun rightestleft::"'a tree=> 'a tree "where
-"rightestleft Tip=Tip"|
+fun rightestleft::"'a tree => 'a tree "where
+"rightestleft Tip = Tip"|
 "rightestleft (Node left x right) =(if right=Tip then left 
                                     else ( (rightestleft right)))  "
 
-fun deltreeroot::"'a tree=>'a tree"where
-"deltreeroot Tip=Tip "|
+fun deltreeroot::"'a tree => 'a tree"where
+"deltreeroot Tip = Tip "|
 "deltreeroot (Node left x right) =(if right=Tip then left else if left=Tip then right else(Node (rightestleft left)(rightest left)right ) ) "
 
-fun deltree::"'a::ord=>'a tree=>'a tree"where
+fun deltree::"'a::ord => 'a tree => 'a tree"where
 "deltree a Tip = Tip"|
 "deltree a (Node left x right) =( if a=x then(deltreeroot(Node left x right))
                                   else if a<x then (Node (deltree a left )x right) 
